27ece6aea292461e2e3f74a810ab9c14923ccad8
nnnlog <sorisem4106@naver.com>
2021-12-11 16:19:25 +0900

    Allow CORS from all sites

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 process.env.TZ = "Asia/Seoul";
 
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+const cors = require("cors");
+app.use(cors()); // for development
 
 require("./lib/db").then(async () => {
 	require("./lib/db").collection("tables").createIndex({time: 1}, {expireAfterSeconds: 60 * 60 * 24 * 14}); // 14 days
